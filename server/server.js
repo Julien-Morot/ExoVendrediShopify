3ac00fb1a53d7c3777e0cf8ccfc5a65d8a005e4a
Julien Morot <julien.morot@devinci.fr>
2019-11-15 11:52:07 +0100

    Second commit

--- server/server.js
+++ server/server.js
@@ -31,9 +31,7 @@ app.prepare().then(() => {
         //Auth token and shop available in session
         //Redirect to shop upon auth
         const { shop, accessToken } = ctx.session;
-        ctx.cookies.set("shopOrigin", shop, {
-          httpOnly: false
-        });
+        ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
         ctx.redirect("/");
       }
     })
